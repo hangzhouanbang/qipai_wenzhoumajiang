8d9759db3633971dbe12c1d9b68917f362b69d4a
YaphetS <xjzsdfz@outlook.com>
2018-12-12 11:43:44 +0800

    在线人数统计第一版

--- src/main/java/com/anbang/qipai/wenzhoumajiang/cqrs/q/dbo/PanActionFrameDbo.java
+++ src/main/java/com/anbang/qipai/wenzhoumajiang/cqrs/q/dbo/PanActionFrameDbo.java
@@ -1,7 +1,14 @@
 package com.anbang.qipai.wenzhoumajiang.cqrs.q.dbo;
 
 import com.dml.majiang.pan.frame.PanActionFrame;
+import org.springframework.data.mongodb.core.index.CompoundIndex;
+import org.springframework.data.mongodb.core.index.CompoundIndexes;
+import org.springframework.data.mongodb.core.mapping.Document;
 
+@Document
+@CompoundIndexes({
+        @CompoundIndex(name = "gameId_1_panNo_1", def = "{'gameId': 1, 'panNo': 1}")
+})
 public class PanActionFrameDbo {
 
 	private String id;
--- src/main/java/com/anbang/qipai/wenzhoumajiang/msg/msjobj/MajiangHistoricalJuResult.java
+++ src/main/java/com/anbang/qipai/wenzhoumajiang/msg/msjobj/MajiangHistoricalJuResult.java
@@ -16,7 +16,10 @@ public class MajiangHistoricalJuResult {
 	private int panshu;
 	private long finishTime;
 
-	public MajiangHistoricalJuResult(JuResultDbo juResultDbo, MajiangGameDbo majiangGameDbo) {
+    public MajiangHistoricalJuResult() {
+    }
+
+    public MajiangHistoricalJuResult(JuResultDbo juResultDbo, MajiangGameDbo majiangGameDbo) {
 		gameId = juResultDbo.getGameId();
 		WenzhouMajiangJuResult wenzhouMajiangJuResult = juResultDbo.getJuResult();
 		dayingjiaId = wenzhouMajiangJuResult.getDayingjiaId();
