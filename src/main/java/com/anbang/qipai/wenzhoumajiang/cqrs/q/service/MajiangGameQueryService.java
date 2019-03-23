6e275638d59729579e8a742734abc9b65b188298
ethan0353 <yinziyu7@gmail.com>
2019-03-23 09:56:25 +0800

    fix

--- src/main/java/com/anbang/qipai/wenzhoumajiang/cqrs/q/service/MajiangGameQueryService.java
+++ src/main/java/com/anbang/qipai/wenzhoumajiang/cqrs/q/service/MajiangGameQueryService.java
@@ -170,7 +170,6 @@ public class MajiangGameQueryService {
 			return record;
 		}
 
-		watchRecord.getWatchers().add(watcher);
 		for (Watcher list : watchRecord.getWatchers()) {
 			if (list.getId().equals(watcher.getId())) {
 				list.setState(watcher.getState());
@@ -184,8 +183,11 @@ public class MajiangGameQueryService {
 		return watchRecord;
 	}
 
+	/**
+	 * 查询观战中的玩家
+	 */
 	public boolean findByPlayerId(String gameId, String playerId) {
-		if (watchRecordDao.findByPlayerId(gameId, playerId) != null) {
+		if (watchRecordDao.findByPlayerId(gameId, playerId, "join") != null) {
 			return true;
 		}
 		return false;
